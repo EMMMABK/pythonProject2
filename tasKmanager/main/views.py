059ca1a753024e74f06dca18901492853ff85c17
Адиль <adilbikiev2@gmail.com>
2022-06-15 00:16:51 +0600

    Initial commit

---
+++ tasKmanager/main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Main page of this site', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form was wrong'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+
+
